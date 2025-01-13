dd8efcabd02364a85f7d840ec404df6d5702b723
aryan12-afk <aryanbhandari04@gmail.com>
2025-01-13 21:30:33 +0530

    Flask server code

--- server.py
+++ server.py
@@ -1,5 +1,18 @@
-From Flask 
-import Flask; 
-request; 
-jsonify
+
+from flask import flask; request; jsonify
+import util
+
 app=flask(__name__)
+
+@app.route('/Get_location_name')
+def Get_location_name():
+    response=jsonify({
+        'Locations':util.get_location_names()
+    })
+    response.headers.add('Access-control-allow-origin', '*')
+
+    return response
+
+if __name__=="__main__":
+    print('Starting Python Flask server For Home Pricing Prediction....')
+    app.run()
