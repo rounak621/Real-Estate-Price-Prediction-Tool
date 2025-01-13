9c3034e672d5a982f353f55e64dac770920526eb
aryan12-afk <aryanbhandari04@gmail.com>
2025-01-13 22:12:17 +0530

    Coding in progress

--- util.py
+++ util.py
@@ -7,11 +7,15 @@ __data_columns=None
 __model=None
 
 def get_estimated_price(locations,sqft,bhk,bath):
-    loc_index=__data_columns.index(location.lower())
-    x = np.zeros(len(x.columns))
+    try:
+        loc_index=__data_columns.index(location.lower())
+    except:
+         loc_index = -1
+
+    x = np.zeros(len(__data_columns))
     x[0]=sqft
-    x[1]=bhk
-    x[2]=bath
+    x[1]=bath
+    x[2]=bhk
     if loc_index >= 0:
          x[loc_index]=1
     return __model.predict([x])
