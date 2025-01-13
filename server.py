159becf7639e9c957e6a3f6a98d4221ab4eed86b
aryan12-afk <aryanbhandari04@gmail.com>
2025-01-13 22:36:23 +0530

    server code updated

--- server.py
+++ server.py
@@ -13,6 +13,19 @@ def Get_location_name():
 
     return response
 
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    total_sqft=float(request.form['total_sqft'])
+    location=request.form['location']
+    bhk=int(request.form['bhk'])
+    bath=int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+    })
+
+    return response
+
 if __name__=="__main__":
     print('Starting Python Flask server For Home Pricing Prediction....')
     app.run()
